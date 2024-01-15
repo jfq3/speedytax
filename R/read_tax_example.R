a5aba59a332da5af1dfc7b6cc3ad8735cbfef8ad
John Quensen <quensenj@msu.edu>
2024-01-15 08:44:24 -0500

    Modifiy example in read_tax_example according to the renamed example classification files.

--- R/read_tax_example.R
+++ R/read_tax_example.R
@@ -7,7 +7,7 @@
 #' @export
 #' @examples
 #' read_tax_example()
-#' read_tax_example("qiime2_class_table.tsv")
+#' read_tax_example("qiime2_classification_table.tsv")
 read_tax_example <- function(file = NULL) {
   if (is.null(file)) {
     dir(system.file("extdata", package = "speedytax"))
