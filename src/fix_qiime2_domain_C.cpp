792cda95eddbcd606343a22a18c092ac74a582fa
John Quensen <quensenj@msu.edu>
2024-01-15 22:31:09 -0500

    Changed bitwise OR "|" OR to logical OR "||"

--- src/fix_qiime2_domain_C.cpp
+++ src/fix_qiime2_domain_C.cpp
@@ -5,7 +5,7 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 StringMatrix fix_qiime2_domain_C(StringMatrix sorted_data) {
   for(int i = 0; i < sorted_data.nrow(); i++) {
-    if ((sorted_data(i, 0) == "") | (sorted_data(i, 0) == "Unassigned")) {
+    if ((sorted_data(i, 0) == "") || (sorted_data(i, 0) == "Unassigned")) {
       sorted_data(i, 0) = "uncl_Domain";
     }
   }
