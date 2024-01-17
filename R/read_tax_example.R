48fb5c3d939f1e2b57c62d88cc41fbcdb1d89c1d
John Quensen <quensenj@msu.edu>
2024-01-17 15:04:29 -0500

    Documented read_tax_example, esp. returns

--- R/read_tax_example.R
+++ R/read_tax_example.R
@@ -1,10 +1,9 @@
 #' Get path to example taxonomy files
 #'
-#' speedytax comes bundled with example taxonomy files in its `inst/extdata`
-#' directory. This function make them easy to access
+#' 'speedytax' comes bundled with example taxonomy files in its `inst/extdata` directory. This function makes them easy to access.
 #'
-#' @param file Name of file. If `NULL`, the example files will be listed.
-#' @returns Returns the full paath to the requested file from the package sub-directory inst/extdata if it exists. If called without specifying a file, it returns a list of files in inst/extdata. This function has the added benefit of working during package development when the system.file method cannot be used.
+#' @param file Name of file to return. If `NULL`, the files in inst/extdata will be listed.
+#' @returns Returns the full path to the requested file from the package sub-directory inst/extdata if it exists. If called without specifying a file, it returns a list of files in inst/extdata. This function has the added benefit of working during package development when the system.file method cannot be used.
 #' @export
 #' @examples
 #' read_tax_example()
